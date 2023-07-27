4af6c35f79e955283067cb25e9d2ea1113432e9a
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-27 10:57:41 +0200

    implement Part 1: Render Stars

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -8,6 +8,18 @@ function renderStars() {
 
   //--v-- your code here --v--
 
+  for (let srat = 1; srat <= 5; srat++) {
+    const imgElement = document.createElement("img");
+
+    //version1 to creat img
+    // imgElement.setAttribute("src", "assets/star-empty.svg");
+
+    //version2 to creat img
+    imgElement.src = "assets/star-empty.svg";
+    starContainer.append(imgElement);
+  }
+  console.log("Number of stars rendered:", starContainer.children.length);
+
   //--^-- your code here --^--
 }
 
