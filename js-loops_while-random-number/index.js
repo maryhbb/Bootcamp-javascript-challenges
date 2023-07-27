621fbb25f816039fd9fa05364165854e19606405
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-27 10:27:00 +0200

    Implement Task js-loops/while-random-number

--- js-loops_while-random-number/index.js
+++ js-loops_while-random-number/index.js
@@ -7,8 +7,15 @@ let count = 0;
 
 // --v-- write/change code here --v--
 
+while (number <= 0.9) {
+  count++;
+  console.log(number);
+  number = Math.random()
+}
+
 // --^-- write/change code here --^--
 
 console.log(
   `The number that ended the loop was ${number} and it took ${count} rounds to do this!`
 );
+
