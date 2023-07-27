6ba5a168c50d70a8738b3e13413b145e93e228e0
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-27 11:15:19 +0200

    Implement Part 2: Change number of filled stars

--- js-loops_stars/js/index.js
+++ js-loops_stars/js/index.js
@@ -2,25 +2,28 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
 
-  for (let srat = 1; srat <= 5; srat++) {
+  for (let star = 1; star <= 5; star++) {
     const imgElement = document.createElement("img");
 
-    //version1 to creat img
+    //version1 to create img
     // imgElement.setAttribute("src", "assets/star-empty.svg");
 
-    //version2 to creat img
-    imgElement.src = "assets/star-empty.svg";
+    //version2 to create img
+    // imgElement.src = "assets/star-empty.svg";
+
+    imgElement.src =
+      star <= filledStars ? "assets/star-filled.svg" : "assets/star-empty.svg";
+
     starContainer.append(imgElement);
   }
-  console.log("Number of stars rendered:", starContainer.children.length);
 
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(4);
