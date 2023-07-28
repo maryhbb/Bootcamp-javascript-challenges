05ff44727541e1eeb4733c08e2ed9f9d4dafa6ce
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-28 11:12:57 +0200

    Implement Task2  js-callback-functions user-login-callback-2-with-argument

--- js-callback-functions_user-login-callback-2-with-argument/js/index.js
+++ js-callback-functions_user-login-callback-2-with-argument/js/index.js
@@ -5,3 +5,13 @@ function handleUserLogin(onSuccess) {
 }
 
 // The exercise starts here!
+
+function showWelcomeMessage(userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+}
+
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(function (userName) {
+  console.log(`Welcome ${userName}! You are logged in now.`);
+});
