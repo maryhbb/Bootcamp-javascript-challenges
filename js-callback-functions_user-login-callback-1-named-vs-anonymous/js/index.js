be0a4a8aaa53b6472d9881262cbd36d7f627a74b
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-28 10:10:21 +0200

    Implement Task1-  js-callback-functions user-login-callback-1-named-vs-anonymous

--- js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
+++ js-callback-functions_user-login-callback-1-named-vs-anonymous/js/index.js
@@ -5,4 +5,14 @@ function handleUserLogin(onSuccess) {
   onSuccess();
 }
 
+function showWelcomeMessage() {
+  console.log("Welcome! You are logged in now");
+}
+
 // Call handleUserLogin below!
+handleUserLogin(showWelcomeMessage);
+
+handleUserLogin(() => {
+  // console.log("Welcome! You are logged in now");
+  showWelcomeMessage();
+});
