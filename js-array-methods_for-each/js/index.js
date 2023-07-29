fefa089783fd42f30113b9102c0b8d242e0a00cb
Mary <76174247+maryhbb@users.noreply.github.com>
2023-07-30 00:17:58 +0200

    Implement js-array-methods/for-each task

--- js-array-methods_for-each/js/index.js
+++ js-array-methods_for-each/js/index.js
@@ -19,4 +19,9 @@ const colors = [
   "#A5A5A5",
 ];
 
-colors.forEach();
+colors.forEach((color) => {
+  colorElement = document.createElement("div");
+  colorElement.classList.add("color-box");
+  colorElement.style.backgroundColor = color;
+  document.body.append(colorElement);
+});
