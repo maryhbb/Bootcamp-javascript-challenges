8b0db755b363b35c7ac13931d7ba08c9b013fa28
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-02 12:33:54 +0200

    Implement js-structure/shapes task

---
+++ js-structure_shapes/components/Circle/Circle.js
@@ -0,0 +1,9 @@
+import { getRandomColor } from "../../utils/randomColor.js";
+export function Circle() {
+  const circle = document.createElement("div");
+  circle.classList.add("circle");
+  circle.addEventListener("click", () => {
+    circle.style.backgroundColor = getRandomColor();
+  });
+  return circle;
+}
--- js-structure_shapes/index.js
+++ js-structure_shapes/index.js
@@ -1,36 +1,13 @@
-import { getRandomColor } from "./utils/randomColor.js";
+import { Circle } from "./components/Circle/Circle.js";
+import { Square } from "./components/Square/Square.js";
+import { Pentagon } from "./components/Pentagon/Pentagon.js";
+
 console.clear();
 
 const root = document.getElementById("root");
 
-function Circle() {
-  const circle = document.createElement("div");
-  circle.classList.add("circle");
-  circle.addEventListener("click", () => {
-    circle.style.backgroundColor = getRandomColor();
-  });
-  return circle;
-}
 const circle = Circle();
-
-function Square() {
-  const square = document.createElement("div");
-  square.classList.add("square");
-  square.addEventListener("click", () => {
-    square.style.backgroundColor = getRandomColor();
-  });
-  return square;
-}
 const square = Square();
-
-function Pentagon() {
-  const pentagon = document.createElement("div");
-  pentagon.classList.add("pentagon");
-  pentagon.addEventListener("click", () => {
-    pentagon.style.backgroundColor = getRandomColor();
-  });
-  return pentagon;
-}
 const pentagon = Pentagon();
 
 root.append(circle, square, pentagon);
