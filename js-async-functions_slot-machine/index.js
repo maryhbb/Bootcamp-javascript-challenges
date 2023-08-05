5c317fd0a63dfc09ca16fb4c7b80c4f5404f32c3
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-06 01:47:09 +0200

    Implement js-async-functions/slot-machine task

--- js-async-functions_slot-machine/index.js
+++ js-async-functions_slot-machine/index.js
@@ -2,6 +2,7 @@ import { Wheel } from "./components/Wheel/Wheel.js";
 import { SpinButton } from "./components/SpinButton/SpinButton.js";
 import { Machine } from "./components/Machine/Machine.js";
 import { Result } from "./components/Result/Result.js";
+import { getMaxCount } from "./utils/symbols.js";
 
 console.clear();
 
@@ -22,6 +23,23 @@ root.append(machine, spinButton, result);
 //                                      ↙️
 spinButton.addEventListener("click", async () => {
   spinButton.disabled = true;
+  result.setSpinning();
+
+  try {
+    const values = await Promise.all([
+      wheel1.spin(),
+      wheel2.spin(),
+      wheel3.spin(),
+    ]);
+    const maxCount = getMaxCount(values);
+
+    const newPoints = maxCount === 3 ? 100 : maxCount === 2 ? 10 : 0;
+    result.setResult(newPoints);
+  } catch {
+    result.setMachineChoked();
+  } finally {
+    spinButton.disabled = false;
+  }
   /**
    * Hint 1:
    * The wheel elements have a spin method that returns a promise.
@@ -62,8 +80,6 @@ spinButton.addEventListener("click", async () => {
    * and make sure it is always executed after the wheels have stopped,
    * even if an error was thrown.
    */
-
-  spinButton.disabled = false;
 });
 
 /**
