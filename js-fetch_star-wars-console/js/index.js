3d732df824ba3f669bd464022aa4c79bd11acaf5
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-06 18:19:57 +0200

    Implement js-fetch/star-wars-console task

--- js-fetch_star-wars-console/js/index.js
+++ js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,22 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      // Success (Good Response)
+      const data = await response.json();
+      console.log(data.results);
+      console.log(`the eye color of R2-D2: ${data.results[2].eye_color}`);
+    } else {
+      // Failure (Bad Response)
+      console.error("Bad Response");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 fetchData();
