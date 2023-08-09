d6af59b8ebf7c13dc0e9fbae2d4b57c91830c287
Mary <76174247+maryhbb@users.noreply.github.com>
2023-08-09 09:36:09 +0200

    Implement js-modern-syntax/ destructuring-objects task

--- js-modern-syntax_destructuring-objects/index.js
+++ js-modern-syntax_destructuring-objects/index.js
@@ -12,6 +12,8 @@ const company = {
 
 // example: export const {value1} = myObject;
 
+export const { course } = company;
+
 // EXERCISE 2
 // We have an object called 'user'.
 // Write the destructuring assignment that reads:
@@ -20,6 +22,7 @@ const company = {
 //  2.3: 'isAdmin' property into the variable 'isAdmin' (false, if no such property exists)
 
 const user = { name: "John", years: 30 };
+export const { name: name, years: age, isAdmin = false } = user;
 
 // EXERCISE 3
 // Extract the variables from the dog object and
@@ -31,6 +34,8 @@ const dog = {
   age: 5,
 };
 
+export const { name: dogName, breed: dogBreed, age: dogAge } = dog;
+
 // EXERCISE 4
 // Extract the 'lastName' property from the person object as "personLastName".
 // Store all other properties in an object called "moreInformation".
@@ -40,3 +45,5 @@ const person = {
   age: 24,
   firstName: "Alex",
 };
+
+export const { lastName: personLastName, ...moreInformation } = person;
